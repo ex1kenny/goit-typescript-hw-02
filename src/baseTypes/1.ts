5777badc1e2262f5190bd2e5c6c9331f66105643
ex1kenny <ex1kenny4543@gmail.com>
2024-04-08 21:37:30 +0300

    home-work2

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,12 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
 
 let age = 50;
-let name = 'Max';
+let name = "Max";
 let toggle = true;
 let empty = null;
 let notInitialize;
-let callback = (a) => { return 100 + a };
+let callback = (a: number): number => {
+  return 100 + a;
+};
 
 export {};
