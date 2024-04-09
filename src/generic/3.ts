855ca8d9e6ed75068c58750e010c3eef01e0985e
ex1kenny <ex1kenny4543@gmail.com>
2024-04-09 14:21:18 +0300

    home-work2.0

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,7 +3,7 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T extends object, K extends object>(objB: K, objA: T): any {
+function merge<T extends object, K extends object>(objB: K, objA: T): object {
   return Object.assign(objA, objB);
 }
 
